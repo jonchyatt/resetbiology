70ae383eeb72989bbee5678a4e2e2357b97665f0
Jon Hyatt <jonchyatt@gmail.com>
2025-09-17 11:09:06 -0400

    fix: Update Stripe API version to 2025-08-27.basil for compatibility

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -2,5 +2,5 @@ import Stripe from 'stripe';
 
 export const stripe =
   process.env.STRIPE_SECRET_KEY
-    ? new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2024-06-20' })
+    ? new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2025-08-27.basil' })
     : null;
